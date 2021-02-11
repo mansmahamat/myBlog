b9f0d1eaa985406c8acbb27b71b220c843b50ccb
mansmahamat <mansour@web-id.fr>
2021-02-11 10:37:00 +0100

    add anamytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -45,9 +45,9 @@ module.exports = {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
         // The property ID; the tracking code won't be generated without it
-        trackingId: "G-ZYG5VT1HCM",
+        trackingId: "G-Y496F1DRS1",
         // Defines where to place the tracking script - `true` in the head and `false` in the body
-        head: true,
+        head: false,
         // Setting this parameter is optional
         anonymize: true,
         // Setting this parameter is also optional
